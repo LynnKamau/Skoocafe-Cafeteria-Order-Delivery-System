bc1f159aecfdb0d0e36c0e7735d219b2d27d003b
Lynn Kamau <98352438+LynnKamau@users.noreply.github.com>
2022-12-27 14:51:01 +0300

    Add files via upload

---
+++ partials/_footer.php
@@ -0,0 +1,3 @@
+<div class="footer container-fluid bg-dark text-light">
+    <p class="text-center py-2 mb-0">Copyright © 2022 Designed by <a href="https://outlook.office365.com/mail/" target="_blank" rel="noopener noreferrer">lkamau7@usiu.ac.ke</a></p>
+</div>
